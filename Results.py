d6b0ee454795bf25c184814e38ded1359bb614b9
Shane Leon Smullen <SHANE.SMULLEN.2017@mumail.ie>
2019-12-05 18:23:40 +0000

    partially working result file gen

--- Results.py
+++ Results.py
@@ -2,7 +2,7 @@ import gensim
 from sense2vec import Sense2Vec
 
 
-def word_vec_query(verb, *args, **compositions):
+def word_vec_query(verb, topn, *args, **compositions):
     if args:
         positive = []
         negative = []
@@ -12,7 +12,7 @@ def word_vec_query(verb, *args, **compositions):
             if key == 'n':
                 negative = key
         return w2v.most_similar(positive, negative)
-    return w2v.most_similar(verb)
+    return w2v.most_similar(verb, topn)
 
 
 def sense_vec_query(verb):
@@ -23,73 +23,74 @@ def sense_vec_query(verb):
     return s2v.most_similar(query, n=3)
 
 
-results = open("D:\Results\\results.txt", "w")
-w2v_model = "D:\WikiData\Trained\word2vec\\10GB\word2vec.model"
-s2v_model = "D:\WikiData\Trained\sense2vec\RedditVecs\sense2vec-vectors"
+results = open("X:\Thesis\Results\\results.txt", "w")
+w2v_model = "X:\Thesis\word2vec.model"
+s2v_model = "X:\Thesis\sense2vec-vectors"
 
 w2v = gensim.models.Word2Vec.load(w2v_model)
 s2v = Sense2Vec().from_disk(s2v_model)
 
 ########################################################################################################################
 
-results.write("sense2vec")
-results.write("Word lists")
+results.write("sense2vec" + "\n")
+results.write("Word lists" + "\n")
 
 ########################################################################################################################
-results.write("word2vec")
-results.write("Word lists")
+results.write("word2vec" + "\n")
+results.write("Word lists" + "\n")
 write_string = ""
-write_string += ("2similarity('walked', 'talk') " + str(w2v.similarity('walked', 'talk')))
-write_string += ("2similarity('walked', 'slept') " + str(w2v.similarity('walked', 'slept')))
-write_string += ("2model2.similarity('run', 'walk') " + str(w2v.similarity('run', 'walk')))
-write_string += ("2model2.similarity('run', 'drive') " + str(w2v.similarity('run', 'drive')))
-write_string += ("2model2.similarity('run', 'talk') " + str(w2v.similarity('run', 'talk')))
-write_string += ("2model2.similarity('run', 'hatter') " + str(w2v.similarity('run', 'hatter')))
-write_string += ("2model2.similarity('run', 'think') " + str(w2v.similarity('run', 'think')))
+write_string += ("Similarity('walked', 'talk') " + str(w2v.similarity('walked', 'talk'))) + "\n"
+write_string += ("Similarity('walked', 'slept') " + str(w2v.similarity('walked', 'slept'))) + "\n"
+write_string += ("Similarity('run', 'walk') " + str(w2v.similarity('run', 'walk'))) + "\n"
+write_string += ("Similarity('run', 'drive') " + str(w2v.similarity('run', 'drive'))) + "\n"
+write_string += ("Similarity('run', 'talk') " + str(w2v.similarity('run', 'talk'))) + "\n"
+write_string += ("Similarity('run', 'hatter') " + str(w2v.similarity('run', 'hatter'))) + "\n"
+write_string += ("Similarity('run', 'think') " + str(w2v.similarity('run', 'think'))) + "\n"
 
-write_string += ("Most similar")
-write_string += ("Most similar 'run' " + str(word_vec_query("run")))
-write_string += ("Most similar 'walked' " + str(word_vec_query("walked")))
-write_string += ("Most similar 'jogged' " + str(word_vec_query("jogged")))
-write_string += ("Most similar 'talked' " + str(word_vec_query("talked")))
-write_string += ("Most similar 'slept' " + str(word_vec_query("slept")))
+write_string += ("Most similar" + "\n")
+write_string += ("Most similar 'run' " + str(w2v.most_similar("run", topn=3))) + "\n"
+write_string += ("Most similar 'walked' " + str(w2v.most_similar("walked", topn=3))) + "\n"
+write_string += ("Most similar 'jogged' " + str(w2v.most_similar("jogged", topn=3))) + "\n"
+write_string += ("Most similar 'talked' " + str(w2v.most_similar("talked", topn=3))) + "\n"
+write_string += ("Most similar 'slept' " + str(w2v.most_similar("slept", topn=3))) + "\n"
 
-write_string += ("Logical entailment")
-write_string += ("Compositions")
-write_string += ("'take' + 'pay'" + str(word_vec_query(None, p=["take", "pay"])))
+write_string += ("Logical entailment" + "\n")
+write_string += ("Compositions" + "\n")
+write_string += ("'take' + 'pay'" + str(w2v.most_similar(["take", "pay"], topn=3))) + "\n"
 
-write_string += ("Opposites")
-write_string += ("'buy' - 'sell'" + str(word_vec_query(None, p=["buy"], n=["sell"])))
-write_string += ("'get' - 'give'" + str(word_vec_query(None, p=["get"], n=["give"])))
-write_string += ("Difference: 'buy' - 'sell' ~ 'get' - 'give'")
-write_string +=  + str((word_vec_query(None, p=["buy"], n=["sell"])[0] - word_vec_query(None, p=["get"], n=["give"])[0]))
+write_string += ("Opposites" + "\n")
+write_string += ("'buy' - 'sell'" + str(w2v.most_similar(["buy"], ["sell"], topn=3))) + "\n"
+write_string += ("'get' - 'give'" + str(w2v.most_similar(["get"], ["give"], topn=3))) + "\n"
+#write_string += ("Difference: 'buy' - 'sell' ~ 'get' - 'give'")
+#write_string +=  + str((word_vec_query(None, p=["buy"], n=["sell"])[0] - word_vec_query(None, p=["get"], n=["give"])[0]))
+results.write(str(write_string))
 
 ########################################################################################################################
 
-results.write("sense2vec")
-results.write("Word lists")
+results.write("sense2vec" + "\n")
+results.write("Word lists" + "\n")
 write_string = ""
-write_string += ("2similarity('walked', 'talk') " + str(s2v.similarity('walked', 'talk')))
-write_string += ("2similarity('walked', 'slept') " + str(s2v.similarity('walked', 'slept')))
-write_string += ("2model2.similarity('run', 'walk') " + str(s2v.similarity('run', 'walk')))
-write_string += ("2model2.similarity('run', 'drive') " + str(s2v.similarity('run', 'drive')))
-write_string += ("2model2.similarity('run', 'talk') " + str(s2v.similarity('run', 'talk')))
-write_string += ("2model2.similarity('run', 'hatter') " + str(s2v.similarity('run', 'hatter')))
-write_string += ("2model2.similarity('run', 'think') " + str(s2v.similarity('run', 'think')))
-results.wite(str(write_string))
+write_string += ("Similarity('walked', 'talk') " + str(s2v.similarity('walked|VERB', 'talk|VERB'))) + "\n"
+write_string += ("Similarity('walked', 'slept') " + str(s2v.similarity('walked|VERB', 'slept|VERB'))) + "\n"
+write_string += ("Similarity('run', 'walk') " + str(s2v.similarity('run|VERB', 'walk|VERB'))) + "\n"
+write_string += ("Similarity('run', 'drive') " + str(s2v.similarity('run|VERB', 'drive|VERB'))) + "\n"
+write_string += ("Similarity('run', 'talk') " + str(s2v.similarity('run|VERB', 'talk|VERB'))) + "\n"
+write_string += ("Similarity('run', 'hatter') " + str(s2v.similarity('run|VERB', 'hatter|NOUN'))) + "\n"
+write_string += ("Similarity('run', 'think') " + str(s2v.similarity('run|VERB', 'think|VERB'))) + "\n"
+results.write(str(write_string))
 
-results.write("Most similar")
+results.write("Most similar" + "\n")
 write_string = ""
-write_string += ("Most similar 'run' " + str(sense_vec_query("run")))
-write_string += ("Most similar 'walked' " + str(sense_vec_query("walked")))
-write_string += ("Most similar 'jogged' " + str(sense_vec_query("jogged")))
-write_string += ("Most similar 'talked' " + str(sense_vec_query("talked")))
-write_string += ("Most similar 'slept' " + str(sense_vec_query("slept")))
-results.wite(str(write_string))
+write_string += ("Most similar 'run' " + str(sense_vec_query("run|VERB"))) + "\n"
+write_string += ("Most similar 'walked' " + str(sense_vec_query("walked|VERB"))) + "\n"
+write_string += ("Most similar 'jogged' " + str(sense_vec_query("jogged|VERB"))) + "\n"
+write_string += ("Most similar 'talked' " + str(sense_vec_query("talked|VERB"))) + "\n"
+write_string += ("Most similar 'slept' " + str(sense_vec_query("slept|VERB"))) + "\n"
+results.write(str(write_string))
 
-results.write("Logical entailment")
-results.write("Compositions")
+results.write("Logical entailment" + "\n")
+results.write("Compositions" + "\n")
 
-results.write("Opposites")
+results.write("Opposites" + "\n")
 
 ########################################################################################################################
