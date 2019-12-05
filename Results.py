8ed111acc32c80ecdbfa3af12e0160fdf8150c04
u180282 <shane.smullen.2017@mumail.ie>
2019-12-05 16:32:03 +0000

    results write changed

--- Results.py
+++ Results.py
@@ -30,8 +30,6 @@ s2v_model = "D:\WikiData\Trained\sense2vec\RedditVecs\sense2vec-vectors"
 w2v = gensim.models.Word2Vec.load(w2v_model)
 s2v = Sense2Vec().from_disk(s2v_model)
 
-write_string = ""
-
 ########################################################################################################################
 
 results.write("sense2vec")
@@ -40,6 +38,7 @@ results.write("Word lists")
 ########################################################################################################################
 results.write("word2vec")
 results.write("Word lists")
+write_string = ""
 write_string += ("2similarity('walked', 'talk') " + str(w2v.similarity('walked', 'talk')))
 write_string += ("2similarity('walked', 'slept') " + str(w2v.similarity('walked', 'slept')))
 write_string += ("2model2.similarity('run', 'walk') " + str(w2v.similarity('run', 'walk')))
@@ -47,43 +46,46 @@ write_string += ("2model2.similarity('run', 'drive') " + str(w2v.similarity('run
 write_string += ("2model2.similarity('run', 'talk') " + str(w2v.similarity('run', 'talk')))
 write_string += ("2model2.similarity('run', 'hatter') " + str(w2v.similarity('run', 'hatter')))
 write_string += ("2model2.similarity('run', 'think') " + str(w2v.similarity('run', 'think')))
-results.wite(str(write_string))
 
-results.write("Most similar")
-results.write("Most similar 'run' ", word_vec_query("run"))
-results.write("Most similar 'walked' ", word_vec_query("walked"))
-results.write("Most similar 'jogged' ", word_vec_query("jogged"))
-results.write("Most similar 'talked' ", word_vec_query("talked"))
-results.write("Most similar 'slept' ", word_vec_query("slept"))
+write_string += ("Most similar")
+write_string += ("Most similar 'run' " + str(word_vec_query("run")))
+write_string += ("Most similar 'walked' " + str(word_vec_query("walked")))
+write_string += ("Most similar 'jogged' " + str(word_vec_query("jogged")))
+write_string += ("Most similar 'talked' " + str(word_vec_query("talked")))
+write_string += ("Most similar 'slept' " + str(word_vec_query("slept")))
 
-results.write("Logical entailment")
-results.write("Compositions")
-results.write("'take' + 'pay'", word_vec_query(None, p=["take", "pay"]))
+write_string += ("Logical entailment")
+write_string += ("Compositions")
+write_string += ("'take' + 'pay'" + str(word_vec_query(None, p=["take", "pay"])))
 
-results.write("Opposites")
-results.write("'buy' - 'sell'", word_vec_query(None, p=["buy"], n=["sell"]))
-results.write("'get' - 'give'", word_vec_query(None, p=["get"], n=["give"]))
-results.write("Difference: 'buy' - 'sell' ~ 'get' - 'give'")
-results.write(word_vec_query(None, p=["buy"], n=["sell"])[0] - word_vec_query(None, p=["get"], n=["give"])[0])
+write_string += ("Opposites")
+write_string += ("'buy' - 'sell'" + str(word_vec_query(None, p=["buy"], n=["sell"])))
+write_string += ("'get' - 'give'" + str(word_vec_query(None, p=["get"], n=["give"])))
+write_string += ("Difference: 'buy' - 'sell' ~ 'get' - 'give'")
+write_string +=  + str((word_vec_query(None, p=["buy"], n=["sell"])[0] - word_vec_query(None, p=["get"], n=["give"])[0]))
 
 ########################################################################################################################
 
 results.write("sense2vec")
 results.write("Word lists")
-results.write("2similarity('walked', 'talk') ", s2v.similarity('walked', 'talk'))
-results.write("2similarity('walked', 'slept') ", s2v.similarity('walked', 'slept'))
-results.write("2model2.similarity('run', 'walk')", s2v.similarity('run', 'walk'))
-results.write("2model2.similarity('run', 'drive')", s2v.similarity('run', 'drive'))
-results.write("2model2.similarity('run', 'talk')", s2v.similarity('run', 'talk'))
-results.write("2model2.similarity('run', 'hatter')", s2v.similarity('run', 'hatter'))
-results.write("2model2.similarity('run', 'think')", s2v.similarity('run', 'think'))
+write_string = ""
+write_string += ("2similarity('walked', 'talk') " + str(s2v.similarity('walked', 'talk')))
+write_string += ("2similarity('walked', 'slept') " + str(s2v.similarity('walked', 'slept')))
+write_string += ("2model2.similarity('run', 'walk') " + str(s2v.similarity('run', 'walk')))
+write_string += ("2model2.similarity('run', 'drive') " + str(s2v.similarity('run', 'drive')))
+write_string += ("2model2.similarity('run', 'talk') " + str(s2v.similarity('run', 'talk')))
+write_string += ("2model2.similarity('run', 'hatter') " + str(s2v.similarity('run', 'hatter')))
+write_string += ("2model2.similarity('run', 'think') " + str(s2v.similarity('run', 'think')))
+results.wite(str(write_string))
 
 results.write("Most similar")
-results.write("Most similar 'run' ", sense_vec_query("run"))
-results.write("Most similar 'walked' ", sense_vec_query("walked"))
-results.write("Most similar 'jogged' ", sense_vec_query("jogged"))
-results.write("Most similar 'talked' ", sense_vec_query("talked"))
-results.write("Most similar 'slept' ", sense_vec_query("slept"))
+write_string = ""
+write_string += ("Most similar 'run' " + str(sense_vec_query("run")))
+write_string += ("Most similar 'walked' " + str(sense_vec_query("walked")))
+write_string += ("Most similar 'jogged' " + str(sense_vec_query("jogged")))
+write_string += ("Most similar 'talked' " + str(sense_vec_query("talked")))
+write_string += ("Most similar 'slept' " + str(sense_vec_query("slept")))
+results.wite(str(write_string))
 
 results.write("Logical entailment")
 results.write("Compositions")
