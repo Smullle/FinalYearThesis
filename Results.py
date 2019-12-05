6bc28d9bd4412334cd7a9ef71dc662a457fccd15
u180282 <shane.smullen.2017@mumail.ie>
2019-12-05 16:12:28 +0000

    results write changed

--- Results.py
+++ Results.py
@@ -23,12 +23,14 @@ def sense_vec_query(verb):
     return s2v.most_similar(query, n=3)
 
 
-results = open("D:\Results\results.txt", "w")
-w2v_model = "D:\GoogleNews-vectors-negative300.bin"
-s2v_model = "D:\Wiki\Trained\sense2vec\sense2vec"
+results = open("D:\Results\\results.txt", "w")
+w2v_model = "D:\WikiData\Trained\word2vec\\10GB\word2vec.model"
+s2v_model = "D:\WikiData\Trained\sense2vec\RedditVecs\sense2vec-vectors"
 
-w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_model, binary=True)
+w2v = gensim.models.Word2Vec.load(w2v_model)
 s2v = Sense2Vec().from_disk(s2v_model)
+
+write_string = ""
 
 ########################################################################################################################
 
@@ -38,13 +40,14 @@ results.write("Word lists")
 ########################################################################################################################
 results.write("word2vec")
 results.write("Word lists")
-results.write("2similarity('walked', 'talk') ", w2v.similarity('walked', 'talk'))
-results.write("2similarity('walked', 'slept') ", w2v.similarity('walked', 'slept'))
-results.write("2model2.similarity('run', 'walk')", w2v.similarity('run', 'walk'))
-results.write("2model2.similarity('run', 'drive')", w2v.similarity('run', 'drive'))
-results.write("2model2.similarity('run', 'talk')", w2v.similarity('run', 'talk'))
-results.write("2model2.similarity('run', 'hatter')", w2v.similarity('run', 'hatter'))
-results.write("2model2.similarity('run', 'think')", w2v.similarity('run', 'think'))
+write_string += ("2similarity('walked', 'talk') " + str(w2v.similarity('walked', 'talk')))
+write_string += ("2similarity('walked', 'slept') " + str(w2v.similarity('walked', 'slept')))
+write_string += ("2model2.similarity('run', 'walk') " + str(w2v.similarity('run', 'walk')))
+write_string += ("2model2.similarity('run', 'drive') " + str(w2v.similarity('run', 'drive')))
+write_string += ("2model2.similarity('run', 'talk') " + str(w2v.similarity('run', 'talk')))
+write_string += ("2model2.similarity('run', 'hatter') " + str(w2v.similarity('run', 'hatter')))
+write_string += ("2model2.similarity('run', 'think') " + str(w2v.similarity('run', 'think')))
+results.wite(str(write_string))
 
 results.write("Most similar")
 results.write("Most similar 'run' ", word_vec_query("run"))
