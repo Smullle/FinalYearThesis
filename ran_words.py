d69854e1271f66926bfcc523a170e2de75fb26bc
Shane Leon Smullen <SHANE.SMULLEN.2017@mumail.ie>
2020-02-20 17:53:03 +0000

    added w2v to boxplot data

--- ran_words.py
+++ ran_words.py
@@ -8,8 +8,7 @@ import csv
 w2v_model = "D:\\WikiData\\Trained\\word2vec\\10GB\\word2vec.model"
 s2v_model = "D:\\WikiData\\Trained\\sense2vec\\RedditVecs\\sense2vec-vectors"
 
-
-# w2v = gensim.models.Word2Vec.load(w2v_model)
+w2v = gensim.models.Word2Vec.load(w2v_model)
 s2v = Sense2Vec().from_disk(s2v_model)
 
 f = open('D:\\wordlists\\ran_verbs.txt', 'r')
@@ -34,32 +33,15 @@ def wordnet_first_last(word, pos):
         last_word_last_synset = last_synset[-1]
         return [first_word_first_synset, last_word_first_synset, last_word_last_synset]
     except IndexError:
+        # return large value if word not in wordnet
         return [100, 100, 100]
     except KeyError:
         return [100, 100, 100]
 
-# for i in range(50):
-#     verb1 = random.choice(verbs)
-#     verb2 = random.choice(verbs)
-#     print(verb1, verb2, str(s2v.similarity([verb1 + '|VERB'], [verb2 + '|VERB'])))
 
-# for i in range(10):
-#     verb = random.choice(verbs)
-#     wordnet_verbs = wordnet_first_last(verb)
-#     try:
-#         print("Similarity between", verb, "and first word in first synset")
-#         print(verb, wordnet_verbs[0], str(s2v.similarity([verb + '|VERB'], [wordnet_verbs[0] + '|VERB'])))
-#         print("Similarity between", verb, "and last word in first synset")
-#         print(verb, wordnet_verbs[1], str(s2v.similarity([verb + '|VERB'], [wordnet_verbs[1] + '|VERB'])))
-#         print("Similarity between", verb, "and last word in last synset")
-#         print(verb, wordnet_verbs[2], str(s2v.similarity([verb + '|VERB'], [wordnet_verbs[2] + '|VERB'])))
-#     except TypeError:
-#         print("Phrasal Verb found skipping", wordnet_verbs)
-
-
-results = open("D:\\Results\\ran_verbs_results.csv", "w", newline='')
+results_s2v_verb = open("D:\\Results\\s2v_ran_verbs_results.csv", "w", newline='')
 fieldnames = ['verb', 'first word in first synset', 'last word in first synset', 'last word in last synset']
-results_writer = csv.DictWriter(results, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
+results_writer = csv.DictWriter(results_s2v_verb, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
 results_writer.writeheader()
 for i in range(1000):
     verb = random.choice(verbs)
@@ -75,13 +57,14 @@ for i in range(1000):
                                      'last word in first synset': last_first,
                                      'last word in last synset': last_last})
     except TypeError:
+        # ignore phrasal verbs, only single verbs accepted by s2v
         continue
 
-results.close()
+results_s2v_verb.close()
 
-results_noun = open("D:\\Results\\ran_nouns_results.csv", "w", newline='')
+results_s2v_noun = open("D:\\Results\\ran_nouns_results.csv", "w", newline='')
 fieldnames = ['noun', 'first word in first synset', 'last word in first synset', 'last word in last synset']
-results_writer = csv.DictWriter(results_noun, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
+results_writer = csv.DictWriter(results_s2v_noun, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
 results_writer.writeheader()
 for i in range(10000):
     noun = random.choice(nouns)
@@ -97,9 +80,56 @@ for i in range(10000):
                                      'last word in first synset': last_first,
                                      'last word in last synset': last_last})
     except TypeError:
+        # ignore phrasal verbs, only single verbs accepted by s2v
         continue
 
-results_noun.close()
+results_s2v_noun.close()
+
+results_w2v_verbs = open("D:\\Results\\ran_w2v_verbs_results.csv", "w", newline='')
+fieldnames = ['verb', 'first word in first synset', 'last word in first synset', 'last word in last synset']
+results_writer = csv.DictWriter(results_w2v_verbs, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
+results_writer.writeheader()
+for i in range(1000):
+    verb = random.choice(verbs)
+    print(verb)
+    wordnet_verbs = wordnet_first_last(verb, wn.VERB)
+    try:
+        first_first = w2v.similarity(verb, wordnet_verbs[0])
+        last_first = w2v.similarity(verb, wordnet_verbs[1])
+        last_last = w2v.similarity(verb, wordnet_verbs[2])
+        if first_first <= 1 and last_first <= 1 and last_last <= 1:
+            results_writer.writerow({'verb': verb,
+                                     'first word in first synset': first_first,
+                                     'last word in first synset': last_first,
+                                     'last word in last synset': last_last})
+    except TypeError:
+        # ignore phrasal verbs, only single verbs accepted by s2v
+        continue
+
+results_w2v_verbs.close()
+
+results_w2v_noun = open("D:\\Results\\ran_w2v_nouns_results.csv", "w", newline='')
+fieldnames = ['noun', 'first word in first synset', 'last word in first synset', 'last word in last synset']
+results_writer = csv.DictWriter(results_w2v_noun, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
+results_writer.writeheader()
+for i in range(10000):
+    noun = random.choice(nouns)
+    print(noun)
+    wordnet_verbs = wordnet_first_last(noun, wn.NOUN)
+    try:
+        first_first = w2v.similarity(noun, wordnet_verbs[0])
+        last_first = w2v.similarity(noun, wordnet_verbs[1])
+        last_last = w2v.similarity(noun, wordnet_verbs[2])
+        if first_first <= 1 and last_first <= 1 and last_last <= 1:
+            results_writer.writerow({'noun': noun,
+                                     'first word in first synset': first_first,
+                                     'last word in first synset': last_first,
+                                     'last word in last synset': last_last})
+    except TypeError:
+        # ignore phrasal verbs, only single verbs accepted by s2v
+        continue
+
+results_w2v_noun.close()
 
 # sanity check
 # for i in range(1):
