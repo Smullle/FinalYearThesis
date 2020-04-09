0b56ae38bc98df57fd54275f20d4d216f645a4ff
Shane <shanesmullen@gmail.com>
2020-04-09 16:44:56 +0100

    check if word in dictionary

--- ResultsCsv.py
+++ ResultsCsv.py
@@ -19,7 +19,7 @@ def word_vec_query(verb, positive=None, negative=None, topn=1):
     return w2v.most_similar(verb, topn=topn)
 
 
-def sense_vec_query(verb, n=30):
+def sense_vec_query(verb, n=1000):
     query = verb
     assert query in s2v
     # vector = s2v[query]
@@ -28,7 +28,7 @@ def sense_vec_query(verb, n=30):
     ans = []
     for word in word_list:
         word = str(word)
-        if word.split('|')[0] in words.words():
+        if word[1:].split('|')[0][:-1] in words.words():
             ans.append(word)
     return ans
 
