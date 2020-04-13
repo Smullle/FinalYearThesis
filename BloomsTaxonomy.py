ec53ab39eb01fb8b7b57cba2c253f340ea9901dc
Shane <shanesmullen@gmail.com>
2020-04-13 16:05:53 +0100

    Added extra comp to BloomsTaxonomy.py

--- BloomsTaxonomy.py
+++ BloomsTaxonomy.py
@@ -30,27 +30,28 @@ except FileNotFoundError:
 results = open(r_data, "w")
 print("Results File Created at:", r_data)
 
-results.write("Sense2Vec\n")
-# Check each verb against every words in the level, excluding itself
-for cat in categories:
-    for word in cat:
-        comp_words = [w for w in cat if w != word]
-        for comp in comp_words:
-            results.write(str(s2v.similarity([word.lower() + '|VERB'], [comp.lower() + '|VERB'])) + ',')
-    results.write("\n")
-
-results.write("Word2Vec\n")
-for cat in categories:
-    for word in cat:
-        comp_words = [w for w in cat if w != word]
-        for comp in comp_words:
-            results.write(str(w2v.similarity(word.lower(), comp.lower())) + ',')
-    results.write("\n")
+# results.write("Sense2Vec\n")
+# # Check each verb against every words in the level, excluding itself
+# for cat in categories:
+#     for word in cat:
+#         comp_words = [w for w in cat if w != word]
+#         for comp in comp_words:
+#             results.write(str(s2v.similarity([word.lower() + '|VERB'], [comp.lower() + '|VERB'])) + ',')
+#     results.write("\n")
+#
+# results.write("Word2Vec\n")
+# for cat in categories:
+#     for word in cat:
+#         comp_words = [w for w in cat if w != word]
+#         for comp in comp_words:
+#             results.write(str(w2v.similarity(word.lower(), comp.lower())) + ',')
+#     results.write("\n")
 
 # Compare head word in each category
 results.write("Sense2Vec Head\n")
 head = categories[0][0]
 for cat in categories:
+    print(head, cat[0])
     results.write(str(s2v.similarity([head.lower() + '|VERB'], [cat[0].lower() + '|VERB'])) + ',')
 results.write("\n")
 
